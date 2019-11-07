453a269d3c4a9650e681c7a6ddaa874ec8a698ce
Evgeny3101 <e.yakovlev3101@gmail.com>
2019-11-07 12:29:02 +0300

    страница login, адаптация размеров и страница index, правки медиа запросов

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,10 +25,10 @@ module.exports = {
 
   entry: {
     app: `${PATHS.src}/js/index.js`,
-    // login: `${PATHS.src}/js/login.js`,
-    // signUp: `${PATHS.src}/js/sign-up.js`,
-    // searchRoom: `${PATHS.src}/js/search-room.js`,
-    // roomDetails: `${PATHS.src}/js/room-details.js`,
+    login: `${PATHS.src}/js/login.js`,
+    signUp: `${PATHS.src}/js/sign-up.js`,
+    searchRoom: `${PATHS.src}/js/search-room.js`,
+    roomDetails: `${PATHS.src}/js/room-details.js`,
     // uiKit: `${PATHS.src}/js/ui-kit.js`
   },
 
