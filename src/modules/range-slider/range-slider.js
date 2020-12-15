2e5659404990fd1981b919ed41d9cef501e362d7
Evgeny3101 <e.yakovlev3101@gmail.com>
2020-12-15 10:55:08 +0300

    Обновил range-slider.

--- src/modules/range-slider/range-slider.js
+++ src/modules/range-slider/range-slider.js
@@ -1,9 +1,7 @@
 import '../../assets/js/libs/range-slider';
 
 const fields = ['.range-slider__field1', '.range-slider__field2'];
-const valueDOM = document.querySelector('.range-slider__value');
 const fieldDOM = [];
-const values = [];
 
 fields.forEach((field, i) => {
   fieldDOM[i] = document.querySelector(field);
@@ -13,38 +11,13 @@ $('.js-range-slider__container').rangeSlider({
   sliderValues: [5000, 10000],
   sliderType: 'range',
   maxValue: 15500,
+  minValue: 0,
   step: 100,
   textField: fields,
+
+  updateValues(values) {
+    const valuesArr = values.map((name) => `${name.toLocaleString()}₽`);
+    valuesArr[0] += ' - ';
+    return valuesArr;
+  },
 });
-
-// редактирование значений при создании
-fieldDOM.forEach((item, i) => {
-  const field = item;
-
-  field.style.display = 'none';
-  values[i] = Number(item.innerText).toLocaleString();
-});
-
-valueDOM.innerText = `${values[0]}₽ - ${values[1]}₽`;
-
-// событие на обновление значений страницы
-function handleFieldMutation(mutation) {
-  mutation.forEach((item, i) => {
-    values[i] = Number(item.target.innerText).toLocaleString();
-  });
-
-  valueDOM.innerText = `${values[0]}₽ - ${values[1]}₽`;
-}
-const observer = new MutationObserver((mutations) => handleFieldMutation(mutations));
-
-// настраиваем наблюдатель
-const config = {
-  attributes: false,
-  childList: true,
-  characterData: true,
-  subtree: false,
-};
-
-// установка событий
-observer.observe(fieldDOM[0], config);
-observer.observe(fieldDOM[1], config);
